e385f9eff6731606495da904d1cfa67f8313527f
Haixiang <haixiang6123@gmail.com>
2018-11-15 11:23:06 -0800

    Update: carousel auto play

--- Carousel/js/index.js
+++ Carousel/js/index.js
@@ -12,6 +12,7 @@ function makeCurrent($node) {
 function makeLeave($node) {
     return $node.removeClass('enter current').addClass('leave')
 }
+
 function makeEnter($node) {
     return $node.removeClass('leave current').addClass('enter')
 }
@@ -24,20 +25,32 @@ function findIndex(n) {
     return n % 3 + 1
 }
 
+function autoPlay() {
+    // Set clock
+    return setInterval(() => {
+        // Leave current window
+        makeLeave(getSlide(n))
+            .one('transitionend', (event) => {
+                // Make it to enter state when it finishes the transition
+                makeEnter($(event.currentTarget))
+            })
+
+        // Next slide will get into the window
+        makeCurrent(getSlide(n + 1))
+
+        n += 1
+    }, 2000)
+}
+
 // Initialize classes
 init()
 
-// Set clock
-setInterval(() => {
-    // Leave current window
-    makeLeave(getSlide(n))
-        .one('transitionend', (event) => {
-            // Make it to enter state when it finishes the transition
-            makeEnter($(event.currentTarget))
-        })
+let timer = autoPlay()
 
-    // Next slide will get into the window
-    makeCurrent(getSlide(n + 1))
-
-    n += 1
-}, 2000)
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        window.clearInterval(timer)
+    } else {
+        timer = autoPlay()
+    }
+})
