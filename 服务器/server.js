8ed00cf513f09baa32bf592b7e408fb01a9b7572
Haixiang <haixiang6123@gmail.com>
2018-11-17 09:10:54 -0800

    Update: jsonp

--- 服务器/server.js
+++ 服务器/server.js
@@ -3,16 +3,18 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
+if (!port) {
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
-  var path = request.url 
+  var path = request.url
   var query = ''
-  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
+  if (path.indexOf('?') >= 0) {
+    query = path.substring(path.indexOf('?'))
+  }
   var pathNoQuery = parsedUrl.pathname
   var queryObject = parsedUrl.query
   var method = request.method
@@ -27,32 +29,38 @@ var server = http.createServer(function(request, response){
 
 
 
+  console.log(queryObject)
 
-
-if (path === '/style.css') {
-  response.setHeader('Content-Type', 'text/css; charset=utf-8')
-  response.write('body {background-color: red}')
-  response.end()
-}
-else if (path === '/main.js') {
-  response.setHeader('Content-Type', 'text/css; charset=utf-8')
-  response.write('alert("这是JS执行的")')
-  response.end()
-}
-else if (path === '/') {
-  response.setHeader('Content-Type', 'text/html; charset=utf-8')
-  response.write('<!DOCTYPE>\n<html>' + 
-    '<head><link href="/style.css" rel="stylesheet">' + 
-    '</head><body>' + 
-    '<h1>Hello 你好</h1>' + 
-    '<script src="/main.js"></script>' + 
-    '</body></html>')
-  response.end()
-}
-else {
-  response.statusCode = 404
-  response.end()
-}
+  if (path === '/') {
+    var string = fs.readFileSync('./index.html', 'utf8')
+    var amount = fs.readFileSync('./db.txt', 'utf8')
+    console.log(amount)
+    string = string.replace('&&&amount&&&', amount)
+    response.setHeader('Content-Type', 'text/html; charset=utf-8')
+    response.write(string)
+    response.end()
+  } else if (path.indexOf('/pay') != -1) {
+    console.log('jjj')
+    // var amount = fs.readFileSync('./db.txt', 'utf8')
+    // var newAmount = amount - 1
+    // fs.writeFileSync('./db.txt', newAmount)
+    if (Math.random() < 0.5) {
+response.statusCode = 200
+response.setHeader('Content-Type', 'application/javascript')
+response.write(`
+${queryObject.callback}.call(undefined, {
+    "success": true,
+  })
+`)
+response.end()
+    } else {
+      response.statusCode = 404
+      response.write('fail')
+      response.end()
+    }
+  } else {
+    response.statusCode = 404
+  }
 
 
 
@@ -65,5 +73,3 @@ else {
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
-
